a4e9acbb1f159b623aa75b4488ec66d48eadef28
Christoph Schulz <christoph1@christohulzsair.home>
2016-05-11 21:42:11 +0200

    Erste Testversion der Uhr

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -13,10 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerlabel: UILabel!
     
     @IBOutlet weak var timerbutton: UIButton!
+    
+    var timer:NSTimer?
+    var ms = 0
 
     @IBAction func buttontapped(sender: AnyObject) {
         timerbutton.setTitle("Stopp", forState: .Normal)
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
+    
+        func update() {
+            self.ms += 1
+            timerlabel.text = String(self.ms)
+        }
     
     
 }
