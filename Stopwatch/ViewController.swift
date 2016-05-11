0616d929b5f569a247b969134abc1be80bbe21c6
Christoph Schulz <christoph1@christoph-schulzs-macbook-air.local>
2016-05-11 16:13:47 +0200

    Erste Testbare Version

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var timerlabel: UILabel!
+    
+    @IBOutlet weak var timerbutton: UIButton!
+
+    @IBAction func buttontapped(sender: AnyObject) {
+        timerbutton.setTitle("Stopp", forState: .Application)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
